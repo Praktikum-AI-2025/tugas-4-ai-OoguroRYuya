dba4c401f507c29553ce54cb0dc755173b83d6f3
OoguroRYuya <mhafizfenaldhie2904@gmail.com>
2025-04-15 23:55:50 +0700

    menambah jawaban

--- Tugas 4.py
+++ Tugas 4.py
@@ -41,26 +41,12 @@ def solution_05():
         fill_mode='nearest'
     )
 
-    train_generator= ImageDataGenerator(rescale=1./255)
+    # train_generator= ImageDataGenerator(rescale=1./255)
     
     # YOUR CODE HERE
-    train_generator= train_datagen.flow_from_directory(
-        TRAINING_DIR,
-        target_size=(150, 150),
-        batch_size=32,
-        class_mode='binary'
-    )
 
     model=tf.keras.models.Sequential([
         # YOUR CODE HERE, end with a Neuron Dense, activated by sigmoid
-        tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150,150,3)),
-        tf.keras.layers.MaxPooling2D(2,2),
-        tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2,2),
-        tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2,2),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(512, activation='relu'),
                 tf.keras.layers.Dense(1, activation='sigmoid') #DO NOT CHANGE THIS LINE!
         ])
 
